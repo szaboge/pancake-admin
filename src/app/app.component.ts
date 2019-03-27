0090c99bd157e6a83c4f9b22d25579873805d035
Gergely Szabo <szagege@gmail.com>
2019-03-27 20:00:36 +0100

    feat(pancake-admin): 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from './auth/auth.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  authSub: Subscription;
+  valueChange: Subscription;
+
+  all = 0;
+  inprogress = 0;
+  donee = 0;
+
+  reservations;
+
+  count_statistic(array) {
+    let t_a = 0;
+    let t_i = 0;
+    let t_d = 0;
+    for (const item in array) {
+      if (array.hasOwnProperty(item)) {
+        t_a += this.count(array[item].pancakes);
+        if (array[item].done) {
+          t_d += this.count(array[item].pancakes);
+        } else {
+          t_i += this.count(array[item].pancakes);
+        }
+      }
+    }
+    this.all = t_a;
+    this.inprogress = t_i;
+    this.donee = t_d;
+  }
+
+  constructor(public authService: AuthService) {
+    this.authSub = this.authService.af().authState.subscribe((next) => {
+        if (next) {
+          this.valueChange = this.authService.db().object('reservations').valueChanges().subscribe((val) => {
+            this.reservations = val;
+            this.count_statistic(this.reservations);
+          });
+        } else {
+          this.authService.af().auth.signInAnonymously();
+        }
+      }
+    );
+  }
+
+  done(key: string) {
+    this.authService.db().object('reservations/' + key + '/done/').set(true);
+  }
+
+  notdone(key: string) {
+    this.authService.db().object('reservations/' + key + '/done/').set(false);
+  }
+
+  count(pancakes) {
+    let count = 0;
+    for (const item of pancakes) {
+      count += item.piece;
+    }
+    return count;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {environment} from '../environments/environment';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {MatButtonModule, MatCardModule, MatChipsModule, MatDividerModule, MatExpansionModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
